f8d704df881f745a792ee165a2cea71b1a6cf863
adelaett <90894311+adelaett@users.noreply.github.com>
2024-04-23 13:42:35 +0200

    bench script

--- run-bench.py
+++ run-bench.py
@@ -2,17 +2,30 @@ from subprocess import run
 from pathlib import Path
 import os
 from itertools import product
+import tempfile
+import shutil
 
 env_base = dict(os.environ)
 
+
 for optim, avoid_exceptions in product(["yes", "no"], repeat=2):
-    run(["opam", "reinstall", "../catala-examples/"],
-        check=True, env=env_base | {"NODOC": "yes",
+    tmp_dir = Path(tempfile.mkdtemp())
+    print(
+        f"optim: {optim}, avoid_exceptions: {avoid_exceptions}, directory: {tmp_dir}"
+    )
+    run(["make", "clean", "local-install"],
+        cwd="../catala-examples",
+        capture_output=True,
+        check=True, env=env_base | {"INSTALL_PREFIX": tmp_dir,
+                                    "NODOC": "yes",
                                     "CATALA_FLAGS": "",
                                     "CATALA_TRACE": "no",
                                     "CATALA_OPTIMIZE": optim,
                                     "CATALA_AVOID_EXCEPTIONS": avoid_exceptions}
         )
-    run(["make", "dependencies-python"],
-        check=True)
-    run(["make", "bench_ocaml"], check=True)
+    run(["make", "bench_ocaml"], check=True,
+        env=env_base | {"OCAMLPATH": tmp_dir/"lib"})
+
+    print()
+
+    shutil.rmtree(tmp_dir)
